2f1252cdbe057ad412b18c7dfe223d8d94978029
Your Name <you@example.com>
2024-07-01 21:39:15 +0300

    refactor our code

--- lib/views/widgets/all_expenses.dart
+++ lib/views/widgets/all_expenses.dart
@@ -1,24 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_dash_board/models/all_expenses_item_model.dart';
-import 'package:responsive_dash_board/utils/app_images.dart';
 import 'package:responsive_dash_board/views/widgets/all_expenses_header.dart';
-import 'package:responsive_dash_board/views/widgets/all_expenses_item.dart';
 import 'package:responsive_dash_board/views/widgets/all_expenses_item_list_view.dart';
+import 'package:responsive_dash_board/views/widgets/custom_background_container.dart';
 
 class AllExpenses extends StatelessWidget {
   const AllExpenses({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      decoration: ShapeDecoration(
-        color: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12),
-        ),
-      ),
-      child: const Column(
+    return const CustomBackgroundContainer(
+      padding: 20,
+      child: Column(
         children: [
           AllExpensesHeader(),
           SizedBox(
