c727b9fb3c821c43368c6f3d000ad568411d66c5
Your Name <you@example.com>
2024-07-04 16:57:23 +0300

    transaction history done

--- lib/views/widgets/transaction_history.dart
+++ lib/views/widgets/transaction_history.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/utils/app_styles.dart';
+import 'package:responsive_dash_board/views/widgets/transaction_history_list_view.dart';
 
 class TransactionHistory extends StatelessWidget {
   const TransactionHistory({super.key});
@@ -9,27 +10,39 @@ class TransactionHistory extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const Text('Transaction History', style: AppStyles.styleSimiBold20),
-            Text('See all',
-                style: AppStyles.styleMedium16
-                    .copyWith(color: const Color(0xff4EB7F2))),
-          ],
-        ),
+        const TransactionHistoryHeader(),
         const SizedBox(
           height: 20,
         ),
-        Text('13 April 2024',
-            style: AppStyles.styleMedium16.copyWith(
-              color: const Color(0xFFAAAAAA),
-            ),),
+        Text(
+          '13 April 2024',
+          style: AppStyles.styleMedium16.copyWith(
+            color: const Color(0xFFAAAAAA),
+          ),
+        ),
         const SizedBox(
           height: 16,
-          
         ),
-        const Text('13 April 2024',
+        const TransactionHistoryListView(),
+      ],
+    );
+  }
+}
+
+class TransactionHistoryHeader extends StatelessWidget {
+  const TransactionHistoryHeader({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const Text('Transaction History', style: AppStyles.styleSimiBold20),
+        Text('See all',
+            style: AppStyles.styleMedium16
+                .copyWith(color: const Color(0xff4EB7F2))),
       ],
     );
   }
---
+++ lib/views/widgets/transaction_item.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/models/transation_model.dart';
+import 'package:responsive_dash_board/utils/app_styles.dart';
+
+class TransactionItem extends StatelessWidget {
+  const TransactionItem({super.key, required this.transactionModel});
+
+  final TransactionModel transactionModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: const Color(0xffFAFAFA),
+      elevation: 0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: ListTile(
+        title: Text(
+          transactionModel.title,
+          style: AppStyles.styleSimiBold16.copyWith(
+            color: const Color(0xFF064060),
+          ),
+        ),
+        subtitle: Text(
+          transactionModel.date,
+          style: AppStyles.styleRegular16.copyWith(
+            color: const Color(0xFFAAAAAA),
+          ),
+        ),
+        trailing: Text(transactionModel.amount,
+            style: AppStyles.styleSimiBold16.copyWith(
+              color: transactionModel.isWithdrawal
+                  ? const Color(0xFFF3735E)
+                  : const Color(0xFF7CD87A),
+            )),
+      ),
+    );
+  }
+}
