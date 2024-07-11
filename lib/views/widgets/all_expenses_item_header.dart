8dc5cff992d9dc92565f9604b370f64b2c5fd017
Your Name <you@example.com>
2024-07-11 17:52:43 +0300

    done refactor our code.

--- lib/views/widgets/all_expenses_item_header.dart
+++ lib/views/widgets/all_expenses_item_header.dart
@@ -11,19 +11,25 @@ class AllExpensesItemHeader extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Container(
-          width: 60,
-          height: 60,
-          decoration: ShapeDecoration(
-            color: imageBackGround ?? const Color(0xFFFAFAFA),
-            shape: const OvalBorder(),
+        Flexible(
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(maxHeight: 60, maxWidth: 60 ),
+            child: AspectRatio(
+              aspectRatio: 1,
+              child: Container(
+                decoration: ShapeDecoration(
+                  color: imageBackGround ?? const Color(0xFFFAFAFA),
+                  shape: const OvalBorder(),
+                ),
+                child: Center(
+                    child: SvgPicture.asset(
+                  image,
+                  colorFilter: ColorFilter.mode(
+                      imageColor ?? const Color(0xFF4EB7F2), BlendMode.srcIn),
+                )),
+              ),
+            ),
           ),
-          child: Center(
-              child: SvgPicture.asset(
-            image,
-            colorFilter: ColorFilter.mode(
-                imageColor ?? const Color(0xFF4EB7F2), BlendMode.srcIn),
-          )),
         ),
         const Expanded(child: SizedBox()),
         Transform.rotate(
